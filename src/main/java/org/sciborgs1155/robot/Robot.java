687a8f5d82acd9d28aa6889ba14200bad734b6ef
metacoderminecraft <metacoderminecraft@gmail.com>
2024-08-01 17:05:43 -0400

    removing old drive

--- src/main/java/org/sciborgs1155/robot/Robot.java
+++ src/main/java/org/sciborgs1155/robot/Robot.java
@@ -1,9 +1,5 @@
 package org.sciborgs1155.robot;
 
-import static edu.wpi.first.units.Units.MetersPerSecond;
-import static edu.wpi.first.units.Units.MetersPerSecondPerSecond;
-import static edu.wpi.first.units.Units.RadiansPerSecond;
-import static edu.wpi.first.units.Units.Second;
 import static edu.wpi.first.wpilibj2.command.button.RobotModeTriggers.*;
 
 import edu.wpi.first.wpilibj.DataLogManager;
@@ -20,8 +16,6 @@ import org.sciborgs1155.lib.FaultLogger;
 import org.sciborgs1155.lib.InputStream;
 import org.sciborgs1155.robot.Ports.OI;
 import org.sciborgs1155.robot.commands.Autos;
-import org.sciborgs1155.robot.drive.Drive;
-import org.sciborgs1155.robot.drive.DriveConstants;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -36,12 +30,9 @@ public class Robot extends CommandRobot implements Logged {
   private final CommandXboxController driver = new CommandXboxController(OI.DRIVER);
 
   // SUBSYSTEMS
-  private final Drive drive = Drive.create();
 
   // COMMANDS
   @Log.NT private final Autos autos = new Autos();
-
-  @Log.NT private double speedMultiplier = Constants.FULL_SPEED;
 
   /** The robot contains subsystems, OI devices, and commands. */
   public Robot() {
@@ -72,7 +63,6 @@ public class Robot extends CommandRobot implements Logged {
         .deadband(Constants.DEADBAND, 1)
         .negate()
         .scale(maxSpeed)
-        .scale(() -> speedMultiplier)
         .signedPow(2)
         .rateLimit(maxRate);
   }
@@ -81,32 +71,11 @@ public class Robot extends CommandRobot implements Logged {
    * Configures subsystem default commands. Default commands are scheduled when no other command is
    * running on a subsystem.
    */
-  private void configureSubsystemDefaults() {
-    drive.setDefaultCommand(
-        drive.drive(
-            createJoystickStream(
-                driver::getLeftX,
-                DriveConstants.MAX_SPEED.in(MetersPerSecond),
-                DriveConstants.MAX_ACCEL.in(MetersPerSecondPerSecond)),
-            createJoystickStream(
-                driver::getLeftY,
-                DriveConstants.MAX_SPEED.in(MetersPerSecond),
-                DriveConstants.MAX_ACCEL.in(MetersPerSecondPerSecond)),
-            createJoystickStream(
-                driver::getRightX,
-                DriveConstants.MAX_ANGULAR_SPEED.in(RadiansPerSecond),
-                DriveConstants.MAX_ANGULAR_ACCEL.in(RadiansPerSecond.per(Second)))));
-  }
+  private void configureSubsystemDefaults() {}
 
   /** Configures trigger -> command bindings */
   private void configureBindings() {
     autonomous().whileTrue(new ProxyCommand(autos::get));
     FaultLogger.onFailing(f -> Commands.print(f.toString()));
-
-    driver
-        .leftBumper()
-        .or(driver.rightBumper())
-        .onTrue(Commands.runOnce(() -> speedMultiplier = Constants.FULL_SPEED))
-        .onFalse(Commands.run(() -> speedMultiplier = Constants.SLOW_SPEED));
   }
 }
